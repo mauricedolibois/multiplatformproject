762d0ea2788cafda22a16a3ff7d8e6fa7e9db7e6
Artres102 <150268118+Artres102@users.noreply.github.com>
2024-10-28 17:55:09 +0000

    It kinda works

--- Assets/Scripts/Guards/EnemyFoV.cs
+++ Assets/Scripts/Guards/EnemyFoV.cs
@@ -2,14 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Numerics;
 using UnityEngine;
+using UnityEngine.AI;
 using Vector3 = UnityEngine.Vector3;
 
 public class EnemyFoV : MonoBehaviour
 {
     [SerializeField] private float fov = 90f;
     [SerializeField] private float viewDistance = 50f;
-    [SerializeField] private Vector3 viewDirection;
+    [SerializeField] private Vector3 viewDirection; // Current direction navMeshAgent.destination - transform.position
     [SerializeField] private GameObject player;
+    private NavMeshAgent NavMeshAgent;
     
     // Start is called before the first frame update
     void Start()
@@ -20,7 +22,7 @@ public class EnemyFoV : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        FindPlayerTarget();
     }
 
     private void FindPlayerTarget()
@@ -36,15 +38,25 @@ public class EnemyFoV : MonoBehaviour
                     if (raycastHit.collider.CompareTag("Player"))
                     {
                         //Player is being seen
-                        Debug.Log(raycastHit.collider.name);
+                        Debug.Log("This is the player");
                     }
-                    else
+                    else if (raycastHit.collider.CompareTag("Enemy"))
                     {
+                        Debug.Log("This is the enemy!");
+                    } else {
                         //It hit something else
-                        Debug.Log(raycastHit.collider.name);
+                        Debug.Log("Object not player lmao");
                     }
                 }
             }
+        }
+    }
+
+    private void GetDirection()
+    {
+        if (NavMeshAgent.destination != Vector3.zero)
+        {
+            viewDirection = NavMeshAgent.destination - transform.position;
         }
     }
 }
