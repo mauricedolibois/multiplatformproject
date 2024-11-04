82211489ef0e2c2fc0388db1b1b18b085f1bc50d
Artres102 <150268118+Artres102@users.noreply.github.com>
2024-11-04 15:05:53 +0000

    Detection system finalized

--- Assets/Scripts/Enemies/EnemyFoV.cs
+++ Assets/Scripts/Enemies/EnemyFoV.cs
@@ -12,6 +12,7 @@ public class EnemyFoV : MonoBehaviour
     [SerializeField] private Vector3 viewDirection; // Current direction navMeshAgent.destination - transform.position
     private GameObject player;
     private NavMeshAgent NavMeshAgent;
+    private float suspicionLevel = 0f;
     
     // Start is called before the first frame update
     void Start()
@@ -30,23 +31,32 @@ public class EnemyFoV : MonoBehaviour
     
     public int FindPlayerTarget()
     {
-        if (Vector3.Distance(transform.position, player.transform.position) < viewDistance)
+        if (Vector3.Distance(transform.position, player.transform.position) < viewDistance) // If the player is close enough activate the code
         {
-            Vector3 directionToPlayer = (player.transform.position - transform.position).normalized;
-            if (Vector3.Angle(viewDirection, directionToPlayer) > fov / 2)
+            Vector3 directionToPlayer = (player.transform.position - transform.position).normalized; // Check direction to the player
+            if (Vector3.Angle(viewDirection, directionToPlayer) > fov / 2) //Check if the direction to the player is the same to the direction the enemy is facing
             {
-                RaycastHit2D raycastHit = Physics2D.Raycast(transform.position, directionToPlayer, viewDistance);
+                RaycastHit2D raycastHit = Physics2D.Raycast(transform.position, directionToPlayer, viewDistance); // Cast a ray in the direction of the player
                 if (raycastHit.collider != null)
                 {
                     if (raycastHit.collider.CompareTag("Player"))
                     {
-                        //Player is being seen
-                        Debug.Log("This is the player");
-                        return (int)EGuardState.Alarmed;
+                        // If the player is directly being seen, raises suspicion level
+                        suspicionLevel = raiseSuspicion(suspicionLevel);
+                        Debug.Log($"This is the player and suspicion level is {suspicionLevel}");
+                        if (suspicionLevel >= 100f)
+                        {
+                            // If the suspicion level is equal or higher than 100, enemy has fully detected the player and it's game over
+                            return (int)EGuardState.Alarmed;
+                        } else if (suspicionLevel >= 50f)
+                        {
+                            // If the suspicion level is equal or higher than 50, then enemy is suspicious and will walk in the direction of the player
+                            return (int)EGuardState.Suspicious;
+                        }
                     } else {
                         //It hit something else
                         Debug.Log(raycastHit.collider.name);
-                        return (int)EGuardState.Suspicious;
+                        return (int)EGuardState.Invalid;
                     }
                 }
             }
@@ -58,10 +68,15 @@ public class EnemyFoV : MonoBehaviour
     {
         if (NavMeshAgent.destination != transform.position)
         {
-            // Update viewDirection angle to the enemies current direction
+            // Update viewDirection angle to the enemies current facing direction
             return viewDirection = GetComponent<Rigidbody2D>().velocity - GetComponent<Rigidbody2D>().position;
         }
         // Keeps the same viewDirection angle
         return viewDirection;
+    }
+
+    private float raiseSuspicion(float suspicion)
+    {
+        return suspicion + 15 * Time.deltaTime;
     }
 }
