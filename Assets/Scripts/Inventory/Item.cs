ca5c1df0d911a6bfc42b3a3d0311f0485b6faa41
9Gazz9 <150268658+9Gazz9@users.noreply.github.com>
2024-11-13 15:38:10 +0000

    Item respawning after reloading scene fixed

--- Assets/Scripts/Inventory/Item.cs
+++ Assets/Scripts/Inventory/Item.cs
@@ -1,40 +1,46 @@
 using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.EventSystems;
 
 public class Item : MonoBehaviour
 {
-    [SerializeField]
-    private string itemName;
-    
-    [SerializeField]
-    private int quantity;
+    [SerializeField] private string itemName;
+    [SerializeField] private int quantity;
+    [SerializeField] private Sprite sprite;
+    [TextArea] [SerializeField] private string itemDescription;
 
-    [SerializeField] 
-    private Sprite sprite;
-    
-    [TextArea]
-    [SerializeField] 
-    private string itemDescription;
-    
     private InventoryManagement inventoryManager;
-    // Start is called before the first frame update
+
+    // ID to track if the item was already picked up
+    [SerializeField] private string uniqueItemID;
+
     void Start()
     {
         inventoryManager = GameObject.Find("UICanvas").GetComponent<InventoryManagement>();
+
+        // Check if the item was already picked up
+        if (PlayerPrefs.GetInt(uniqueItemID, 0) == 1)
+        {
+            Destroy(gameObject); // Destroy item if it was already picked up
+        }
     }
-    
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
             int leftOverItems = inventoryManager.AddItem(itemName, quantity, sprite, itemDescription);
             if (leftOverItems <= 0)
+            {
+                // Mark item as picked up 
+                PlayerPrefs.SetInt(uniqueItemID, 1);
+                PlayerPrefs.Save(); 
+                
                 Destroy(gameObject);
-            else 
+            }
+            else
+            {
                 quantity = leftOverItems;
+            }
         }
     }
 }
