5fcee210468b97ef352fbe1bf85777c945a67b43
Maurice Dolibois <maurice@dolibois.de>
2024-12-16 17:50:22 +0000

    added RestAPI file

---
+++ Assets/Scripts/CompanionApp/RestAPI.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RestAPI : MonoBehaviour
+{
+    // post current roomID to server, get Frequency for certain roomID, get input from companion app, get sessionId and game code
+    
+}
