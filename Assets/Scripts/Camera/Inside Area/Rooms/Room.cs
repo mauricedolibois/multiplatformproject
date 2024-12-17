7281e6093b40e09005f4e45b8b2c380d287a387b
9Gazz9 <150268658+9Gazz9@users.noreply.github.com>
2024-12-17 16:22:23 +0000

    Lab second part

--- Assets/Scripts/Camera/Inside Area/Rooms/Room.cs
+++ Assets/Scripts/Camera/Inside Area/Rooms/Room.cs
@@ -7,7 +7,6 @@ public class Room : MonoBehaviour
 {
     public string roomName;
     public int roomId;
-    [SerializeField] private string serverUrl = "http://localhost:8000";
 
     void OnTriggerEnter2D(Collider2D other)
     {
@@ -16,35 +15,12 @@ public class Room : MonoBehaviour
             Bounds triggerBounds = GetComponent<Collider2D>().bounds;
 
             Camera.main.GetComponent<InsideCamera>().UpdateCameraBounds(triggerBounds.min, triggerBounds.max);
-            
-            StartCoroutine(SendRoomDataToServer());
+
+            //RestAPI.postCurrentRoom(roomId);
         }
     }
 
-    IEnumerator SendRoomDataToServer()
-    {
-        // Create JSON data
-        RoomData roomData = new RoomData { roomName = this.roomName, roomId = this.roomId };
-        string jsonData = JsonUtility.ToJson(roomData);
-
-        // Create a POST request
-        UnityWebRequest request = new UnityWebRequest(serverUrl, "POST");
-        byte[] bodyRaw = System.Text.Encoding.UTF8.GetBytes(jsonData);
-        request.uploadHandler = new UploadHandlerRaw(bodyRaw);
-        request.downloadHandler = new DownloadHandlerBuffer();
-        request.SetRequestHeader("Content-Type", "application/json");
-        
-        yield return request.SendWebRequest();
-
-        if (request.result == UnityWebRequest.Result.Success)
-        {
-            Debug.Log("Room data sent successfully: " + request.downloadHandler.text);
-        }
-        else
-        {
-            Debug.LogError("Failed to send room data: " + request.error);
-        }
-    }
+ 
 
     [Serializable]
     public class RoomData
